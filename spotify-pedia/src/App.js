197f667f99e39ea7d7b196761b22f959c5e93a4f
Zakaria <zakaria.nassreddine@insa-lyon.fr>
2020-11-10 09:28:35 +0100

    Create react app

---
+++ spotify-pedia/src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import TrackTable from './components/TrackTable'
+
+function App() {
+  return <TrackTable/>;
+}
+
+export default App;
---
+++ spotify-pedia/src/components/TrackTable.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+  },
+});
+
+export default function TrackTable() {
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Name</StyledTableCell>
+            <StyledTableCell align="right">Genres</StyledTableCell>
+            <StyledTableCell align="right">Albums</StyledTableCell>
+            <StyledTableCell align="right">Artists</StyledTableCell>
+            <StyledTableCell align="right">Release Dates</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
